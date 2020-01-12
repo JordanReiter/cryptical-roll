17b90336cb7cbc9a7f24d5cb2606f83dbc07cf6d
Jordan Reiter <Jordan_Reiter@comcast.com>
2020-01-11 19:47:11 -0500

    Switch to a class

--- crypt_roll/utils.py
+++ crypt_roll/utils.py
@@ -1,33 +1,34 @@
 import re
 import secrets
 
-def parse_roll(input):
-    count, diceFaces = input.split('d')
-    try:
-        diceFaces, modifier = re.split('[-+]', diceFaces)
-        modifier = int(modifier)
-        if f'-{modifier}' in input:
-            modifier *= -1
-    except ValueError:
-        modifier = None
-    if not count:
-        count = 1
-    count = int(count)
-    dice = count * [int(diceFaces)]
-    if modifier is not None:
-        return (dice, modifier)
-    return dice
+class CryptRoll:
+    def parse_roll(self, input):
+        count, diceFaces = input.split('d')
+        try:
+            diceFaces, modifier = re.split('[-+]', diceFaces)
+            modifier = int(modifier)
+            if f'-{modifier}' in input:
+                modifier *= -1
+        except ValueError:
+            modifier = None
+        if not count:
+            count = 1
+        count = int(count)
+        dice = count * [int(diceFaces)]
+        if modifier is not None:
+            return (dice, modifier)
+        return dice
 
-def roll_die(faces):
-    return secrets.randbelow(faces) + 1
+    def roll_die(self, faces):
+        return secrets.randbelow(faces) + 1
 
-def roll_dice(input):
-    return [
-        roll_die(ii)
-        for ii in input
-    ]
+    def roll_dice(self, input):
+        return [
+            self.roll_die(ii)
+            for ii in input
+        ]
 
-def get_roll_result(dice, modifier=None):
-    modifier = modifier or 0
-    dice_sum = sum(roll_dice(dice))
-    return dice_sum + modifier
+    def get_roll_result(self, dice, modifier=None):
+        modifier = modifier or 0
+        dice_sum = sum(self.roll_dice(dice))
+        return dice_sum + modifier
--- tests/tests.py
+++ tests/tests.py
@@ -1,38 +1,38 @@
 from unittest import TestCase, mock
 
-from crypt_roll.utils import parse_roll
+from crypt_roll.utils import CryptRoll
 
 class TestParse(TestCase):
     def test_simple_d6(self):
-        result = parse_roll('d6')
+        result = CryptRoll().parse_roll('d6')
         self.assertEqual(result, [6])
 
     def test_simple_d8(self):
-        result = parse_roll('d8')
+        result = CryptRoll().parse_roll('d8')
         self.assertEqual(result, [8])
 
     def test_simple_d20(self):
-        result = parse_roll('d20')
+        result = CryptRoll().parse_roll('d20')
         self.assertEqual(result, [20])
     
     def test_multiple_dice_d6(self):
-        result = parse_roll('2d6')
+        result = CryptRoll().parse_roll('2d6')
         self.assertEqual(result, [6, 6])
     
     def test_multiple_dice_4d10(self):
-        result = parse_roll('4d10')
+        result = CryptRoll().parse_roll('4d10')
         self.assertEqual(result, [10, 10, 10, 10])
 
     def test_simple_d6_modifier_plus_4(self):
-        result = parse_roll('d6+4')
+        result = CryptRoll().parse_roll('d6+4')
         self.assertEqual(result, ([6], 4))
 
     def test_simple_d6_modifier_minus_7(self):
-        result = parse_roll('d6-7')
+        result = CryptRoll().parse_roll('d6-7')
         self.assertEqual(result, ([6], -7))
 
     def test_multiple_dict_3d6_modifier_plus_2(self):
-        result = parse_roll('3d6+2')
+        result = CryptRoll().parse_roll('3d6+2')
         self.assertEqual(result, ([6, 6, 6], 2))
 
 
@@ -40,10 +40,9 @@ class TestRoll(TestCase):
     def test_roll_d6(self):
         expected_value = 3
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import roll_dice
             # function should add one to random value
             m.return_value = expected_value - 1
-            output = roll_dice([6])
+            output = CryptRoll().roll_dice([6])
         self.assertTrue(m.called)
         self.assertEqual(m.call_count, 1)
         self.assertEqual(m.call_args, mock.call(6))
@@ -52,10 +51,9 @@ class TestRoll(TestCase):
     def test_roll_d8(self):
         expected_value = 7
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import roll_dice
             # function should add one to random value
             m.return_value = expected_value - 1
-            output = roll_dice([8])
+            output = CryptRoll().roll_dice([8])
         self.assertTrue(m.called)
         self.assertEqual(m.call_count, 1)
         self.assertEqual(m.call_args, mock.call(8))
@@ -67,9 +65,8 @@ class TestRoll(TestCase):
         # so remove 1 from secrets.randbelow arg
         rand_results = [ val - 1 for val in expected_values]
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import roll_dice
             m.side_effect = rand_results
-            output = roll_dice([10, 10, 10, 10])
+            output = CryptRoll().roll_dice([10, 10, 10, 10])
         self.assertTrue(m.called)
         self.assertEqual(m.call_count, 4)
         expected_calls = [mock.call(10)] * 4
@@ -80,10 +77,9 @@ class TestRoll(TestCase):
         dice_type = 6
         expected_value = 2
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import get_roll_result
             # function should add one to random value
             m.return_value = expected_value - 1
-            output = get_roll_result([dice_type])
+            output = CryptRoll().get_roll_result([dice_type])
         self.assertTrue(m.called)
         self.assertEqual(m.call_count, 1)
         self.assertEqual(m.call_args, mock.call(dice_type))
@@ -94,10 +90,9 @@ class TestRoll(TestCase):
         modifier = 4
         expected_roll = 2
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import get_roll_result
             # function should add one to random value
             m.return_value = expected_roll - 1
-            output = get_roll_result([dice_type], modifier)
+            output = CryptRoll().get_roll_result([dice_type], modifier)
         self.assertEqual(output, 6)
 
     def test_get_roll_result_simple_d6_minus_modifier(self):
@@ -105,10 +100,9 @@ class TestRoll(TestCase):
         modifier = -1
         expected_roll = 2
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import get_roll_result
             # function should add one to random value
             m.return_value = expected_roll - 1
-            output = get_roll_result([dice_type], modifier)
+            output = CryptRoll().get_roll_result([dice_type], modifier)
         self.assertEqual(output, 1)
 
     def test_get_roll_result_multiple(self):
@@ -118,9 +112,8 @@ class TestRoll(TestCase):
         # so remove 1 from secrets.randbelow arg
         rand_results = [ val - 1 for val in expected_values]
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import get_roll_result
             m.side_effect = rand_results
-            output = get_roll_result(4 * [dice_type])
+            output = CryptRoll().get_roll_result(4 * [dice_type])
         self.assertEqual(output, 20)
 
     def test_get_roll_result_multiple_modifier(self):
@@ -131,7 +124,6 @@ class TestRoll(TestCase):
         # so remove 1 from secrets.randbelow arg
         rand_results = [ val - 1 for val in expected_values]
         with mock.patch('secrets.randbelow') as m:
-            from crypt_roll.utils import get_roll_result
             m.side_effect = rand_results
-            output = get_roll_result(4 * [dice_type], modifier)
+            output = CryptRoll().get_roll_result(4 * [dice_type], modifier)
         self.assertEqual(output, 27)
